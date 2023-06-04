97ed6b3f698a89c1f459f45c95c970502ea7a41c
Hamzah Ahmad <ahmadhamzah500@gmail.com>
2023-06-05 01:28:37 +0500

    feat: adds code compiling using jdoodle api

--- client/src/pages/Editor/index.js
+++ client/src/pages/Editor/index.js
@@ -1,86 +1,131 @@
-import { useContext, useEffect, useState } from 'react'
-import { SocketContext } from '../../context/socket'
-import { useParams, useNavigate } from 'react-router'
-import axios from "axios"
-import styles from "./Editor.module.scss"
-import { Controlled as CodeMirror } from 'react-codemirror2'
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { SERVER_URL } from '../../config'
+import { useContext, useEffect, useState } from "react";
+import { SocketContext } from "../../context/socket";
+import { useParams, useNavigate } from "react-router";
+import axios from "axios";
+import styles from "./Editor.module.scss";
+import { Controlled as CodeMirror } from "react-codemirror2";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { SERVER_URL } from "../../config";
 
-import 'codemirror/mode/javascript/javascript'
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material.css';
+import "codemirror/mode/javascript/javascript";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
 
 const Editor = () => {
-    const socket = useContext(SocketContext)
-    const { roomId } = useParams();
-    const [codeContent, setCodeContent] = useState("")
-    const [consoleOutput, setConsoleOutput] = useState("")
-    const [isError, setIsError] = useState(false)
-    let navigate = useNavigate();
+  const socket = useContext(SocketContext);
+  const { roomId } = useParams();
+  const [codeContent, setCodeContent] = useState("");
+  const [consoleOutput, setConsoleOutput] = useState("");
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [languageCode, setLanguageCode] = useState("nodejs");
+  let navigate = useNavigate();
 
-    useEffect(() => {
-        socket.emit("join-room", roomId)
-        socket.on('code-typed', (data) => {
-            setCodeContent(data)
-        })
-        socket.on('retrieve-data', (data) => setCodeContent(data))
-        return () => {
-            socket.emit("leave-room", roomId)
-        }
-    }, [socket, roomId])
+  useEffect(() => {
+    socket.emit("join-room", roomId);
+    socket.on("code-typed", (data) => {
+      setCodeContent(data);
+    });
+    socket.on("retrieve-data", (data) => setCodeContent(data));
+    return () => {
+      socket.emit("leave-room", roomId);
+    };
+  }, [socket, roomId]);
 
-    const handleChange = (val) => {
-        setCodeContent(val)
-        socket.emit("code-typed", val)
-    }
-    const executeCode = async () => {
-        try {
-            let res = await axios.post(`${SERVER_URL}/execute`, { code: codeContent.replace(/(\r\n|\n|\r)/gm, " ") })
-            if (res.data) {
-                if (res.data.type === "error") {
-                    setIsError(true)
-                    setConsoleOutput(JSON.stringify(res.data.data))
-                } else {
-                    setIsError(false)
-                    setConsoleOutput(JSON.stringify(res.data.data))
-                }
-            }
-        } catch (err) {
-            console.log(err)
+  const handleChange = (val) => {
+    setCodeContent(val);
+    socket.emit("code-typed", val);
+  };
+  const executeCode = async () => {
+    try {
+      setIsLoading(true);
+      let res = await axios.post(`${SERVER_URL}/execute`, {
+        code: codeContent,
+        language_code: languageCode,
+      });
+      if (res.data) {
+        if (res.data.type === "error") {
+          setIsError(true);
+          setConsoleOutput(JSON.stringify(res.data.data) || "");
+        } else {
+          setIsError(false);
+          let consoleOutputRaw = JSON.stringify(res.data.data) || "";
+          consoleOutputRaw.replace(/(\r\n|\r|"|\\n)/gm, "");
+          console.log({ consoleOutputRaw });
+          setConsoleOutput(consoleOutputRaw);
+          // consoleOutput.replace(/(\r\n|\r|"|\\n)/gm, "")
         }
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
     }
-    return (
-        <div className={styles.container}>
-            <div className={styles.invite_text}>
-                <CopyToClipboard text={roomId}>
-                    <button className={styles.roomId_btn}>Copy this coderoom's ID <i className="fa fa-copy" aria-hidden="true"></i></button>
-                </CopyToClipboard>
-                <button onClick={() => navigate("/")} className={styles.exit_btn}><i className="fa fa-sign-out" aria-hidden="true"></i></button>
-            </div>
-            <div className={styles.editor_section}>
-                <CodeMirror
-                    value={codeContent}
-                    options={{
-                        lineWrapping: true,
-                        lint: true,
-                        mode: "javascript",
-                        lineNumbers: true,
-                        theme: "material"
-                    }}
-                    onBeforeChange={(editor, data, value) => {
-                        handleChange(value)
-                    }}
-                />
-            </div>
-            <div className={styles.output_section}>
-                <button className={styles.execute_btn} onClick={executeCode}>
-                    <i className="fa fa-play" style={{ color: "#FFFFFF" }}></i> Run
-                </button>
-                <div className={!isError ? styles.console_output : styles.console_output_error}>{consoleOutput.replace(/(\r\n|\r|"|\\n)/gm, "")}</div>
-            </div>
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.invite_text}>
+        <CopyToClipboard text={roomId}>
+          <button className={styles.roomId_btn}>
+            Copy this coderoom's ID{" "}
+            <i className="fa fa-copy" aria-hidden="true"></i>
+          </button>
+        </CopyToClipboard>
+        <div>
+          <select
+            className={styles.picker}
+            onChange={(e) => setLanguageCode(e.target.value)}
+            defaultValue={"nodejs"}
+          >
+            <option value={"nodejs"}>JavaScript</option>
+            <option value={"python2"}>Python 2</option>
+            <option value={"python3"}>Python 3</option>
+            <option value={"php"}>PHP</option>
+          </select>
+          <button onClick={() => navigate("/")} className={styles.exit_btn}>
+            <i className="fa fa-sign-out" aria-hidden="true"></i>
+          </button>
         </div>
-    )
-}
+      </div>
+      <div className={styles.editor_section}>
+        <CodeMirror
+          value={codeContent}
+          options={{
+            lineWrapping: true,
+            lint: true,
+            mode: "javascript",
+            lineNumbers: true,
+            theme: "material",
+          }}
+          onBeforeChange={(editor, data, value) => {
+            handleChange(value);
+          }}
+        />
+      </div>
+      <div className={styles.output_section}>
+        <button className={styles.execute_btn} onClick={executeCode}>
+          <i className="fa fa-play" style={{ color: "#FFFFFF" }}></i> Run
+        </button>
 
-export default Editor
+        {isLoading ? (
+          <div className={styles.loader_container}>
+            <div className={styles.loader} />
+          </div>
+        ) : (
+          <div
+            className={
+              !isError ? styles.console_output : styles.console_output_error
+            }
+            dangerouslySetInnerHTML={{
+              __html: consoleOutput
+                .replace(/(\\n)/gm, "<br />")
+                .replace(/(")/gm, ""),
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Editor;
--- server/server.js
+++ server/server.js
@@ -1,76 +1,92 @@
-const app = require('express')();
-const server = require('http').createServer(app);
+const app = require("express")();
+const server = require("http").createServer(app);
 const bodyParser = require("body-parser");
-const { v4: uuidv4 } = require('uuid'); 
+const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const { spawn } = require("child_process");
-const cors = require('cors')
-require('dotenv').config()
-
+const cors = require("cors");
+const { default: axios } = require("axios");
+require("dotenv").config();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-// prevent CORS error 
-app.use(cors())
+// prevent CORS error
+app.use(cors());
 
-const io = require('socket.io')(server, {
-    cors: {
-        origin: "*"
-    }
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
-// route to wake Heroku dyno 
+// route to wake Heroku dyno
 app.get("/", (req, res) => {
-    res.status(200).json({message: "Server awake"})
-})
+  console.log(process.env.JDOODLE_CLIEN_ID);
+  console.log(process.env.JDOODLE_CLIENT_SECRET);
+  res.status(200).json({ message: "Server awake" });
+});
 // route to execute code on the server.
-app.post("/execute", (req, res) => {
-    const { code } = req.body;
-    let result;
-    let fileName = uuidv4() + ".js";
-    try {
-        fs.writeFileSync(fileName, code);
-        // spawn a new child process
-        const javaScript = spawn("node", [fileName]);
-        javaScript.stdout.on('data', function (data) {
-            result = { type: "success", data: data.toString() };
-        });
-        javaScript.stderr.on("data", function (data) {
-            result = { type: "error", data: data.toString() };
-        })
-        javaScript.on('close', (code) => {
-            if(fileName) fs.unlinkSync(fileName);
-            res.json(result);
-        });
-    } catch (e) {
-        fs.unlinkSync(fileName)
-        console.log(`Exception occurred!!!`);
+app.post("/execute", async (req, res) => {
+  const credsUsed = await axios.post(
+    "https://api.jdoodle.com/v1/credit-spent",
+    {
+      clientId: process.env.JDOODLE_CLIEN_ID,
+      clientSecret: process.env.JDOODLE_CLIENT_SECRET,
     }
-})
+  );
+  if (credsUsed?.data?.used > 195) {
+    result = {
+      type: "error",
+      data: "API tokens have expired for the day. Please try again later",
+    };
+    return res.json(result);
+  }
+  const { code, language_code } = req.body;
 
+  const program = {
+    script: code,
+    language: language_code,
+    versionIndex: "3",
+    clientId: process.env.JDOODLE_CLIEN_ID,
+    clientSecret: process.env.JDOODLE_CLIENT_SECRET,
+  };
 
-let dataObj = {} //dataObj will be used to provide all the current code in the room to new participants
+  try {
+    let resp = await axios.post("https://api.jdoodle.com/v1/execute", program);
+    if (resp?.data?.error) {
+      return res.json({ type: "error", data: resp?.data?.error });
+    } else {
+      return res.json({ type: "success", data: resp?.data?.output });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.json({ type: "error", data: "Error occured on the serer" });
+  }
+
+});
+
+let dataObj = {}; //dataObj will be used to provide all the current code in the room to new participants
 io.on("connection", (socket) => {
-    socket.on("code-typed", (data) => {
-        socket.to(socket.chatroom).emit('code-typed', data)
-        dataObj[socket.chatroom] = data;
-    })
-    socket.on("join-room", (roomId) => {
-        socket.join(roomId)
-        socket.chatroom = roomId;
-        //retrieve existing room code for new room participants
-        if (dataObj[socket.chatroom]) {
-            socket.emit("retrieve-data", dataObj[socket.chatroom])
-        }
-    })
-    socket.on("leave-room", (roomId) => {
-        socket.leave(roomId)
-        socket.chatroom = "";
-    })
+  socket.on("code-typed", (data) => {
+    socket.to(socket.chatroom).emit("code-typed", data);
+    dataObj[socket.chatroom] = data;
+  });
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+    socket.chatroom = roomId;
+    //retrieve existing room code for new room participants
+    if (dataObj[socket.chatroom]) {
+      socket.emit("retrieve-data", dataObj[socket.chatroom]);
+    }
+  });
+  socket.on("leave-room", (roomId) => {
+    socket.leave(roomId);
+    socket.chatroom = "";
+  });
 });
 
 const PORT = process.env.PORT || 5000;
